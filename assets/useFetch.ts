17bb85ddac50022bfae2b676080a1241e235e7d7
Alvaro_Aquije <gonzalo_6824@outlook.com>
2021-07-20 19:42:51 -0500

    fix(components): :fire: fixed promise bug

--- assets/useFetch.ts
+++ assets/useFetch.ts
@@ -1,7 +1,22 @@
 import axios from "axios";
+import { useEffect, useState } from "react";
 
-export const useFetch = async (url) => {
-  const res = await axios.get(url);
-  console.log(res);
-  return res;
+export const useFetch = (url) => {
+  const [data, setData] = useState([]);
+
+  // BASE_URL=https://api.coinpaprika.com/v1/${params}
+  useEffect(() => {
+    axios
+      // If needed ad params to base URL
+      .get(`${url}`)
+      .then((res) => {
+        setData(res.data);
+        console.log(res.data);
+      })
+      .catch((err) => {
+        alert(err);
+        console.log(err);
+      });
+  }, []);
+  return data;
 };
