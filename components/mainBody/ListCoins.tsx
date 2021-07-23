91504937849c0588c0bca979cee03a47701e990d
Alvaro_Aquije <gonzalo_6824@outlook.com>
2021-07-22 23:26:54 -0500

    refactor(components): :fire: removed unused code

--- components/mainBody/ListCoins.tsx
+++ components/mainBody/ListCoins.tsx
@@ -9,13 +9,7 @@ export const ListCoins: React.FC<Props> = () => {
   const mainData: any[] = useFetch(
     "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage='1h%2C24h%2C7d'"
   );
-  // const images: any[] = useFetch(
-  //   "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
-  // );
 
-  // images.map((i) => {
-  //   return console.log(i.image);
-  // });
   // TODO styling
   return (
     <>
