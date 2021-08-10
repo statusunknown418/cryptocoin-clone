fd0579e01666254aee58769e0ae99fec4ba5dfbf
Alvaro_Aquije <gonzalo_6824@outlook.com>
2021-08-10 00:04:13 -0500

    ci(.config): :wrench: added domain to next.config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ["assets.coingecko.com", "graphs.coinpaprika.com"],
+    domains: [
+      "assets.coingecko.com",
+      "graphs.coinpaprika.com",
+      "avatars.githubusercontent.com",
+    ],
   },
 };
